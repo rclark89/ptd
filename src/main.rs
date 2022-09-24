8af7229273f807f3c74d11cc59219bca0f219637
rclark89 <rossclark89@gmail.com>
2022-09-24 16:03:01 +0100

    updated service info functions

--- src/main.rs
+++ src/main.rs
@@ -7,35 +7,35 @@ use chrono::{TimeZone, Utc, DateTime};
 
 
 fn main() {
-
+// Instantiates objects to be used in email.
 	let position = posname();
-	
 	let engineer = engname();
-	
 	let safe = yesno();
-	
-	services();
-	
-	send();
-	
+	let g = gas();
+	let l = elec();
+	let w = water();
 	let code = randomcode();
-	
 	let dt = datetime();
+	
+	println!("This permit will be emailed to the specified users.");
 	
 	let emailtext = format!(
 	"\nThis email is from an automated inbox. Do not reply. 
 	\nPermit to dig number: {} {} \n
 	\nPosition name: {} \n
 	\nEngineer name: {} \n
+	\nIt is {} \n
 	\nThe following services have been located: \n
-	\nIt is {} \n", 
-	&code, &dt, &position, &engineer, &safe
+	\n{}\n
+	\n{}\n
+	\n{}\n", 
+	&code, &dt, &position, &engineer, &safe, &g, &l, &w //adds details from input functions to email text string.
 	);
 	
 	let email = EmailBuilder::new() //creates new email item
-        .to("---------------")
-		.to("---------------")
-        .from("---------------")
+        .to("------------")
+		.to("------------")
+        .from("------------")
         .subject("Permit to dig")
         .text(emailtext)
 		//.text("This permit applies to {}.",loc)
@@ -44,9 +44,9 @@ fn main() {
         .build()
         .unwrap();
 		
-    let mut mailer = SmtpClient::new_simple("smtp.zoho.eu") //sends email via Smtp - the simplest way of doing this!
+    let mut mailer = SmtpClient::new_simple("------------") //sends email via Smtp - the simplest way of doing this!
         .unwrap()
-        .credentials(Credentials::new("---------------".into(), "---------------".into()))
+        .credentials(Credentials::new("------------".into(), "------------".into())) //Make sure not to expose your credentials.
         .transport();
 
     let result = mailer.send(email.into());
@@ -95,9 +95,6 @@ fn engname() -> String {
 			
 }
 
-fn send() {
-    println!("This permit will be emailed to the specified users.");
-}
 
 fn yesno() -> String {
 	let mut input = String::new();
@@ -121,26 +118,73 @@ fn yesno() -> String {
 		}
 }
 
-fn services() -> Vec<String> {
-    let svcs = ["Gas", "Electricity", "Water"];
-
-    for item in svcs { //add outputsvcs
-		let mut input = String::new();
-		let mut list = vec![];
-		println!("Is {} present? Y/N",item);
-		io::stdin()
-			.read_line(&mut input)
-			.expect("Failed to read line");
-			if input.trim().to_lowercase() == "y" {
-				println!("{} is present",item);
-				&mut list.push(item.to_string());
-			} else if input.trim().to_lowercase() == "n" {
-				println!("{} is not present",item);
-			} else {
-				println!("Invalid input.");
-			}
+fn gas() -> String {
+  
+	let mut ginput = String::new();
+		println!("Is gas present? Y/N");
+	io::stdin()
+		.read_line(&mut ginput)
+		.expect("Failed to read line");
+		if ginput.trim().to_lowercase() == "y" {
+			println!("Gas is present");
+			let outputgas = "Gas is present.".to_string();
+			return outputgas
+		} else if ginput.trim().to_lowercase() == "n" {
+			println!("Gas is not present");
+			let outputgas = "Gas is not present.".to_string();
+			return outputgas
+		} else {
+			println!("Invalid input.");
+			let outputgas = "Invalid input".to_string();
+			return outputgas
 		}
-	
+			
+}
 
+fn elec() -> String {
+	
+	let mut einput = String::new();
+		println!("Is electricity present? Y/N");
+	io::stdin()
+		.read_line(&mut einput)
+		.expect("Failed to read line");
+		if einput.trim().to_lowercase() == "y" {
+			println!("Electricity is present");
+			let outputlec = "Electricity is present.".to_string();
+			return outputlec
+		} else if einput.trim().to_lowercase() == "n" {
+			println!("Electricity is not present");
+			let outputlec = "Electricity is not present.".to_string();
+			return outputlec
+		} else {
+			println!("Invalid input.");
+			let outputlec = "Invalid input".to_string();
+			return outputlec
+		}
+			
+}
+
+fn water() -> String {	
+
+	let mut winput = String::new();
+		println!("Is water present? Y/N");
+	io::stdin()
+		.read_line(&mut winput)
+		.expect("Failed to read line");
+		if winput.trim().to_lowercase() == "y" {
+			println!("Water is present");
+			let outputw = "Water is present.".to_string();
+			return outputw
+		} else if winput.trim().to_lowercase() == "n" {
+			println!("Water is not present");
+			let outputw = "Water is not present.".to_string();
+			return outputw
+		} else {
+			println!("Invalid input.");
+			let outputw = "Invalid input".to_string();
+			return outputw
+		}		
+	
+	
 }
 
